d7e1a1e2a66aa43f39d363b208dcb770f2076c01
clstialdev <rusty75001@gmail.com>
2023-11-02 15:58:28 +0100

    added interface

---
+++ sumo-integrator-master/lib/sumo/libsumo.h
@@ -0,0 +1,10 @@
+#ifndef LIB_SUMO_LIBSUMO_H
+#define LIB_SUMO_LIBSUMO_H
+
+
+#include "lib/sumo/TraCIAPI.h"
+#include "lib/sumo/TraCIConstants.h"
+#include "lib/sumo/TraCIDefs.h"
+
+
+#endif  // LIB_SUMO_LIBSUMO_H
